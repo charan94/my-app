c68bb063cce059fb831d80b6a3a7e95156397b3e
Sai Charan Krishnagiri <sai.charan1994@gmail.com>
2023-08-11 18:58:27 -0400

    Added: Server functionalities.

--- server.js
+++ server.js
@@ -16,11 +16,40 @@ app.get('/users', async (req, res, next) => {
 app.post("/users/add", async (req, res, next) => {
     const users = JSON.parse(fs.readFileSync(path.join(__dirname, "data/users.json")));
     const user = req.body;
-    if(user.firstName && user.lastName) {
+    if (user.firstName && user.lastName) {
         users.push(user);
         fs.writeFileSync(path.join(__dirname, "data/users.json"), JSON.stringify(users));
     }
     return res.status(200).send(users);
+})
+
+app.get("/user/{id}", async (req, res, next) => {
+    const users = JSON.parse(fs.readFileSync(path.join(__dirname, "data/users.json")));
+    const id = req.params.id;
+    const user = users.find(user => user.id === id) ?? null;
+    if (!user) {
+        return res.status(400).send(`User with id - ${id} not found`);
+    }
+    return res.status(200).send(user);
+})
+
+app.put("/user/update/{id}", async (req, res, next) => {
+    const users = JSON.parse(fs.readFileSync(path.join(__dirname, "data/users.json")));
+    const id = req.params.id;
+    const body = req.body;
+    const user = users.find(user => user.id === id);
+    if (!user) {
+        return res.status(400).send(`User with id - ${id} not found`);
+    }
+    const filteredUsers = users.filter(user => user.id !== id);
+    fs.writeFileSync(path.join(__dirname, "data/users.json"), JSON.stringify([...filteredUsers, { body }]));
+});
+
+app.post("/user/remove/{id}", (req, res, next) => {
+    const id = req.params.id;
+    const users = JSON.parse(fs.readFileSync(path.join(__dirname, "data/users.json")));
+    const filteredUsers = users.filter(r => r.id !== id);
+    fs.writeFileSync(path.join(__dirname, "data/users.json"), JSON.stringify(filteredUsers));
 })
 
 app.listen(8080, () => {

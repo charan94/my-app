3a4255014e4653d334638851912f9090a1e7ec25
Sai Charan Krishnagiri <sai.charan1994@gmail.com>
2023-08-11 18:32:23 -0400

    ADDED: Simple express server

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const fs = require('fs');
+const path = require("path");
+
+const app = express();
+app.use(express.json());
+
+app.get('/users', async (req, res, next) => {
+    const users = fs.readFileSync(path.join(__dirname, "data/users.json"))
+    return res.status(200).send(JSON.parse(users));
+});
+
+app.post("/users/add", async (req, res, next) => {
+    const users = JSON.parse(fs.readFileSync(path.join(__dirname, "data/users.json")));
+    const user = req.body;
+    if(user.firstName && user.lastName) {
+        users.push(user);
+        fs.writeFileSync(path.join(__dirname, "data/users.json"), JSON.stringify(users));
+    }
+    return res.status(200).send(users);
+})
+
+app.listen(8080, () => {
+    console.log('server started in 8080 port');
+});
